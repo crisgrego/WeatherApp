53aa9c586bbfba7c969acd854fb7cc51d85e3bfd
crisgrego <crisrgrego@gmail.com>
2019-03-10 23:19:47 +0100

    Change HomeControllerTest

--- WeatherApp.Tests/WeatherControllerTests.cs
+++ WeatherApp.Tests/WeatherControllerTests.cs
@@ -1,15 +1,18 @@
 using System;
 using WeatherApp.Controllers;
+using WeatherApp.Services;
+using WeatherApp.Tests.Mock;
 using Xunit;
 
 namespace WeatherApp.Tests
 {
-    public class HomeControllerTests
+    public class WeatherControllerTests
     {
         [Fact]
         public void CanExecuteIndex()
         {
-            var controller = new HomeController();
+            var weatherProvider = new OpenWeatherMap(Configuration.Default());
+            var controller = new WeatherController(weatherProvider);
             var result = controller.Index();
 
             Assert.NotNull(result);
