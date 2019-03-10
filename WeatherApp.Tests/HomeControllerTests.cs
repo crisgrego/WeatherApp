c04f5190065f7215871a44fb9c2769041343e008
crisgrego <crisrgrego@gmail.com>
2019-03-10 07:09:22 +0100

    Setup the proyect
    Removes views that come by default.
    Adds unitesting project.
    Sets the OpenWeatherMap Api Key  using the secret manager tool.

---
+++ WeatherApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WeatherApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
